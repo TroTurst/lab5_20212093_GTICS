5b442ae3fbd2b866a3f194cdff67ffcc832ac0ab
Nilton Condori <a20220229@pucp.edu.pe>
2025-09-25 20:15:48 -0500

    Primer comit, solo cree el proyecto

---
+++ src/test/java/com/project/lab5_20212093_gtics/Lab520212093GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.lab5_20212093_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab520212093GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
